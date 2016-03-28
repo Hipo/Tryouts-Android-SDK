73b43bf90402c651ef844f8afdaea9b177d9f488
Ahmet Mentes <ahmetmentes@gmail.com>
2016-03-28 14:13:12 +0300

    Project root moved to repo root app module name changed it to sample

---
+++ sample/src/main/java/com/hipo/tryouts/tryouts_sample/MainActivity.java
@@ -0,0 +1,9 @@
+package com.hipo.tryouts.tryouts_sample;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahmetmentes on 3/28/16.
+ */
+public class MainActivity extends AppCompatActivity {
+}
